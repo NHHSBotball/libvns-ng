6cf5a8451472f985cfb2811118b4cf45037faa0e
Aled Cuda <aledvirgil@gmail.com>
2015-11-20 18:54:49 -0800

    more cat

--- penguins.h
+++ penguins.h
@@ -1,2 +1,10 @@
 #ifndef PENGUINOS
 #define PENGUINOS
+
+typedef struct kitteny{
+  Point end1;
+  Point end2;
+  struct *next;
+};
+
+#endif
--- stuff.c
+++ stuff.c
@@ -1,13 +1,16 @@
-void DetectCats(CvArray* input, void* output)
-{
+#include <stdlib.h>
+void *DetectCats(CvArray *input, void *output){
+  struct kitteny *topnode = malloc(sizeof(struct kitteny));
   //Hough int array line squared theta omega pi rho
   CvArray* catDog4;
   Canny(input, catDog4, 50, 200, 3);
   Vec2f* catCats93;
   HoughLines(catDog4, catCats93, CV_PI/180, 100, 0, 0);
 
-  Vec4i kitten52;
+  Vec4i Kitten52;
+  Kitten52 = catCats[0];
+  
 
-  for( size_t i = 0; i < sizeof(catCats93); i++ ){
-    kitten52 = catCats93[i];
+  for( size_t i = 1; i < sizeof(catCats93); i++ ){
+    Kitten52 = catCats93[i];
     
